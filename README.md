76eefbcc1077f59f707af0d8b0bd7e807f348e2b
De Wildt van Reenen <dev@mangledbits.com>
2025-07-13 13:19:45 -0400

    support flatpak permission override

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # chromium-enable-wayland
 
-Script to enable Wayland for Chromium and Electron apps by updating their .desktop file. I could not find a reliable
-.config/some-config-file mechanism that always worked so I resorted this script to easy update the .desktop files
-anytime I had a new app or installed updates which replace the .desktop.
+Script to enable Wayland for Chromium and Electron apps by updating their .desktop file or disabling the X11 socket
+in the Flatpak permissions. I could not find a reliable .config/some-config-file mechanism that always worked so I resorted this script to easy update the .desktop files anytime I had a new app or installed updates which replace the .desktop.
+
+When updating a .desktop file the script will first copy the original file to the same location with a .desktop.bak extension. In the case for flatpak permissions there is already the ability to reset to original provided by flatpak so
+there is no attempt to perform any kind of backup of the original settings. It is helpful to use an application like
+Flatseal to view/update/reset these settins.
 
 To use the script:
 
-- FIRST edit `use-wayland.sh` and update the `config` variable to list the .desktop files and the executable names to update. The default content contains examples of some .desktop files I regularly update
+- Update the apps.conf file to list all the applications and .desktop files you want to update. The default list contains som example apps and you must update it suite your needs
 - run `use-wayland.sh` anytime you have updated an app to re-update all as needed
+
+## Testing
+
+To test if it is working use xeyes and see if the eyes move when the mouse if over the app as suggested
+by: <https://medium.com/@bugaevc/how-to-easily-determine-if-an-app-runs-on-xwayland-or-on-wayland-natively-8191b506ab9a>
--- use-wayland.sh
+++ use-wayland.sh
@@ -1,36 +1,118 @@
 #!/bin/bash
 # Script to add wayland arguments to chromium and electron apps like VSCODE. The script needs to be run anytime you update these
 # apps since the update will replace the .desktop file until such time as the application in question supports wayland by default.
-#
-# To test if it is working use xeyes and see if the eyes move when the mouse if over the app as suggested by:
-#    https://medium.com/@bugaevc/how-to-easily-determine-if-an-app-runs-on-xwayland-or-on-wayland-natively-8191b506ab9a
 
-# This config variable (the provided values are examples) is a variant of a CSV file, each line contains the path the to .desktop
-# file to update as the path and the name of the executable file inside the .desktop file. The executable name is used as
-# a pattern match to know where to insert the arguments
-config="
-#####################################################################################################################################
-Desktop File Path                                                                                 , Executable Name
-#####################################################################################################################################
-/usr/share/applications/code.desktop                                                                              , /usr/share/code/code
-$HOME/.local/share/flatpak/exports/share/applications/com.brave.Browser.desktop                                   , com.brave.Browser
-$HOME/.local/share/flatpak/exports/share/applications/com.slack.Slack.desktop                                     , com.slack.Slack
-$HOME/.local/share/flatpak/exports/share/applications/com.mattermost.Desktop.desktop                              , com.mattermost.Desktop
-$HOME/.local/share/flatpak/exports/share/applications/io.github.ungoogled_software.ungoogled_chromium.desktop     , io.github.ungoogled_software.ungoogled_chromium
-$HOME/.local/share/flatpak/exports/share/applications/com.logseq.Logseq.desktop                                   , com.logseq.Logseq
-"
+source apps.conf
 
-# For each line in the config string process the .desktop file
-while IFS=',' read -r path executable is_electron; do
-  # Skip empty lines or lines starting with #
-  if [[ -z "$path" || "$path" =~ ^# ]]; then
-    continue
+# Process a desktop file
+#   $1 is the path to the desktop file
+#   $2 is the executable name used to know where to insert arguments
+function process_desktop_file() {
+  path="$1"
+  path_backup="$1.bak"
+  executable="$2"
+
+  if [ -e "$path" ]; then
+    # Check if the file was already updated
+    already_updated_pattern="^Exec.*--ozone-platform=wayland"
+    if grep -q -E "$already_updated_pattern" "$path"; then
+      echo -e "    \033[0;33mSKIPPED\033[0m already up to date"
+    else
+      # create backup
+      echo -e "    creating backup file \033[0;90m$path_backup\033[0m"
+      cp "$path" "$path_backup"
+
+      # Add args to use wayland, may need to use sudo if the file is not editable by the current user
+      if [ -w "$path" ]; then
+        sed -i "/^Exec=/s|$executable|${executable} --enable-features=UseOzonePlatform --ozone-platform=wayland |" "$path"
+      else
+        sudo sed -i "/^Exec=/s|$executable|${executable} --enable-features=UseOzonePlatform --ozone-platform=wayland |" "$path"
+      fi
+
+      if [ $? != 0 ]; then
+        echo -e "    \033[0;31mFAILED\033[0m could not update"
+      else
+        echo -e "    \033[0;32mSUCCESS\033[0m added args for wayland, updated the following lines:"
+        grep -E "$already_updated_pattern" "$path" | sed -r 's/(.*)/      \1/'
+      fi
+    fi
+  else
+    echo -e "    \033[0;31mFAILED\033[0m file not found"
   fi
-  # Skip header line
-  ((line_num++))
-  if [[ $line_num -eq 1 ]]; then
-    continue
+}
+
+# Determine the installation type for a flatpak
+#  $1 - name of the flatpak
+#  returns - --system for global install, --users for users install and empty for not found
+function flatpak_install_type() {
+  if flatpak info "$name" --user &>/dev/null; then
+    echo "--user"
+  elif flatpak info "$name" --system &>/dev/null; then
+    echo "--system"
+  else
+    echo -e "    \033[0;31mFAILED\033[0m flatpak not installed"
   fi
+}
+
+# Determine if a flatpak needs its sockets overridden
+#   $1 - type of installation --system or --user
+#   $2 - flatpak name
+function flatpak_needs_override() {
+  flatpak info "$2" $1 --show-permissions | grep -q -E '^sockets=.*x11'
+}
+
+# for testing uncomment the following lines to clear an array and effectively skip that section
+# FLATPAKS=()
+# DESKTOP_FLATPAKS=()
+# DESKTOP_FILES=()
+
+# Process FLATPAKS which contains flatpak apps where we override flatpak settings to only use wayland
+printf "\n\n\033[0;34mFlatpak permission overrides\033[0m"
+for name in "${FLATPAKS[@]}"; do
+  # remove whitespace
+  name=$(echo "$name" | xargs)
+
+  type=$(flatpak_install_type "$name")
+  echo
+  echo -e "  Processing flatpak application \033[0;34m$name\033[0m installed as \033[0;34m$type\033[0m"
+
+  if [ -z "$type" ]; then
+    echo -e "    \033[0;31mFAILED\033[0m flatpak not installed"
+  elif flatpak_needs_override "$type" "$name"; then
+    flatpak override $type --socket=wayland --nosocket=x11 --nosocket=fallback-x11 "$name"
+    echo -e "    \033[0;32mSUCCESS\033[0m updated flatpak sockets to wayland"
+    updated=$(flatpak info "$name" $type --show-permissions | grep -E '^sockets=')
+    echo "      $updated"
+  else
+    echo -e "    \033[0;33mSKIPPED\033[0m already updated"
+  fi
+
+done
+
+# Process DESKTOP_FLATPAKS which contains flatpak apps that must have their .desktop files adjusted
+printf "\n\n\033[0;34mFlatpak .desktop files\033[0m"
+for name in "${DESKTOP_FLATPAKS[@]}"; do
+  # remove whitespace
+  name=$(echo "$name" | xargs)
+
+  type=$(flatpak_install_type "$name")
+  echo
+  echo -e "  Processing .desktop for flatpak \033[0;34m$name\033[0m installed as \033[0;34m$type\033[0m"
+
+  if [ -z "$type" ]; then
+    echo -e "    \033[0;31mFAILED\033[0m flatpak not installed"
+  elif [[ "$type" == "--system" ]]; then
+    process_desktop_file "/var/lib/flatpak/exports/share/applications/$name.desktop" "$name"
+  else
+    process_desktop_file "$HOME/.local/share/flatpak/exports/share/applications/$name.desktop" "$name"
+  fi
+
+done
+
+# For each line in the DESKTOP_FILES array split the command and desktop file location and update it
+printf "\n\n\033[0;34mCustom .desktop files\033[0m"
+for entry in "${DESKTOP_FILES[@]}"; do
+  IFS=':' read -r path executable is_electron <<<"$entry"
 
   # Remove whitespace
   path=$(echo "$path" | xargs)
@@ -38,30 +120,7 @@ while IFS=',' read -r path executable is_electron; do
 
   # process each file and add wayland args to it
   echo
-  echo -e "Processing \033[0;34m$path\033[0m"
+  echo -e "  Processing .desktop file \033[0;34m$path\033[0m"
 
-  if [ -e "$path" ]; then
-    # Check if the file was already updated
-    already_updated_pattern="^Exec.*--ozone-platform=wayland"
-    if grep -q -E "$already_updated_pattern" "$path"; then
-      echo -e "    \033[0;33mSKIPPED\033[0m already up to date nothing"
-    else
-      # Add args to use wayland, may need to use sudo if the file is not editable by the current user
-      if [ -w "$path" ]; then
-        sed -i "/^Exec=/s|\($executable\)|${executable} --enable-features=UseOzonePlatform --ozone-platform=wayland |" "$path"
-      else
-        sudo sed -i "/^Exec=/s|\($executable\)|${executable} --enable-features=UseOzonePlatform --ozone-platform=wayland |" "$path"
-      fi
-
-      if [ $? != 0 ]; then
-        echo -e "    \033[0;31mFAILED\033[0m could not update"
-      else
-        echo -e "    \033[0;32mSUCCESS\033[0m added args for wayland, updated the following lines:"
-        grep -n -E "$already_updated_pattern" "$path" | sed -r 's/\w*([[:digit:]]+:)(.*)/      \1  \2/'
-      fi
-    fi
-  else
-    echo -e "    \033[0;31mFAILED\033[0m file not found"
-  fi
-
-done <<<"$config"
+  process_desktop_file "$path" "$executable"
+done
